64d2620e91f62fa20a0213498e42f3770133dc6c
Davide Benazzi <benazzi.davide@gmail.com>
2020-11-23 15:09:45 +0100

    Generated one email

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('js ok!');
+/**
+ * Generare 10 email e stamparle a schermo utilizzando Vue, Axios 
+ * */ 
+const app = new Vue ({
+    el: '#app',
+    data: {
+
+    },
+    created() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (response) => {
+        // handle success
+        console.log(response.data.response);
+        })
+        .catch( (error) => {
+        // handle error
+        console.log(error);
+        });
+    },
+    methods: {
+
+    },
+});
